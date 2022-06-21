0523f19fac0516fc1477a7c90b994ccd867cf8f9
dghendry <dhendry@uw.edu>
2022-06-20 23:20:34 -0700

    cleaned up format

--- PS-T10.R
+++ PS-T10.R
@@ -16,31 +16,30 @@ X <- c(1,2,3)
 #' @code
 t01 <- sqrt((1+2+3)^2)*2  # 12
 #' @end
-#'
+
 #' @id ?
 #' @msg Using previous variable (initialized by learner)
 #' @code
 t02 <- ((t01 - 12) == 0) # TRUE
 #' @end
-#'
+
 #' @id ?
 #' @msg Using an initial variable (initialized by problem set)
 #' @code
 t03 <- sum(c(6,12,18) == ((X + X) * 3)) == 3 # TRUE
 #' @end
-#'
+
 #' @id ?
 #' @msg Update an initial variable
 #' @code
 X <- 10
 #' @end
-#'
+
 #' @id ?
 #' @msg Is it retrieved correctly?
 #' @code
 t04 <- (10 == X) #TRUE
 #' @end
-#'
 
 #' @id -
 #' @msg Vectors
@@ -50,7 +49,7 @@ t04 <- (10 == X) #TRUE
 #' @code
 t05 <- c(1,2,3,4)
 #' @end
-#'
+
 #' @id ?
 #' @msg Multiple vector by 2
 #' @code
